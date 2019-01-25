94254d7fb9262a256667da0d83d9f5a7c0f21e4a
rileybrady <37843519+rileybrady@users.noreply.github.com>
2019-01-25 14:52:32 -0800

    Add files via upload

---
+++ Best-Repository-Week2/script.js
@@ -0,0 +1,16 @@
+// JavaScript is capitalized using "camel case": https://en.wikipedia.org/wiki/Camel_case
+// Check out the power of the classList property: https://www.w3schools.com/jsref/prop_element_classlist.asp
+
+// "el" stands for the "element" that was just clicked
+// "el" stands for the "element" that was just clicked
+
+function showTrophy(el){
+	document.getElementById("poptart").src = "image/beans.jpg";
+}
+
+function addClass(el) {
+	document.getElementById("yellow").style.backgroundColor = "blue";
+	document.getElementById("red").style.backgroundColor = "yellow";
+	document.getElementById("blue").style.backgroundColor = "green";
+	document.getElementById("green").style.backgroundColor = "red";
+	}
