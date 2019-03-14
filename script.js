073d52b609307174ef2ea10b60e6e9e0b66dfbfa
rileybrady <37843519+rileybrady@users.noreply.github.com>
2019-03-14 12:39:55 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
-// JavaScript is capitalized using "camel case": https://en.wikipedia.org/wiki/Camel_case
-// Check out the power of the classList property: https://www.w3schools.com/jsref/prop_element_classlist.asp
-
-// "el" stands for the "element" that was just clicked
-// "el" stands for the "element" that was just clicked
-
-
-function showTrophy(el){
-	document.getElementById("poptart").src = "image/beans.jpg";
+	a = document.getElementById("one");
+	b = document.getElementById("two");
+	c = document.getElementById("three");
+	d = document.getElementById("arrow2");
+	
+function nextImage(el){  
+		el.src = "images/green.jpg";
+}
+		
+function nextImage2(el){
+	if (el.src.match("images/distopia_2.jpg")){  
+		el.src = "images/green.jpg";
+		}
 }
 
-function addClass(el) {
-	document.getElementById("yellow").style.backgroundColor = "blue";
-	document.getElementById("red").style.backgroundColor = "yellow";
-	document.getElementById("blue").style.backgroundColor = "green";
-	document.getElementById("green").style.backgroundColor = "red";
+function nextImage3(el){
+	if (el.src.match("images/distopia_3.jfif")){  
+		el.src = "images/green.jpg";
+
+		}
+}
+
+function checkYes(el){
+	if (a.src.match("green") && b.src.match("green") && c.src.match("green")){
+		d.classList.remove("opaque");
+	//I believe this is my most complicated piece of Java, the quantity of the code is not much but to me 
+	//it is very complex since it apply's to HTML and CSS attributes.
+
+	} 
+}	
+function changeScreen(el){
+	if (d.src.match("images/arrow.png")){
+		window.location = 'https://cybersonic60963240.github.io/Yay/gameover.html';
 	}
+	else if (d.src.match("images/arrow2.png")){
+		el.src = "images/arrow.png";
+}
+}
